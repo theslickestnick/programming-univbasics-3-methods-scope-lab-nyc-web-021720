4413d50a0ce50a26307b0cd4762a6ceb7d6d3476
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-21 16:14:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,12 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+def link
+  status = "It's Dangerous To Go Alone! Take This."
+  puts status
+end
+
+def any_phrase(phrase)
+  puts "#{}"
+end
